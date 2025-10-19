e7e6d15551d11430b435db4b790dc0f1f29e641f
Rajat Sharma <rajatsharma221098@gmail.com>
2025-10-19 19:47:53 +0530

    add cors to acess on localhost

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
+
+app.use(cors({origin: "*"}));
 
 // Middleware
 app.use(express.json());
