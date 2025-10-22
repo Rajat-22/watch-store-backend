25ca8e41cedcca3b43257fcd75ba4de45737157c
Rajat Sharma <rajatsharma221098@gmail.com>
2025-10-22 12:27:32 +0530

    watch routes

---
+++ routes/watchProductRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require('../controllers/watchProductController');
+
+const { protect, admin } = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware');
+
+// Public route: Get all products
+router.get('/', getAllProducts);
+
+// Admin-only routes: Create, Update, Delete
+// upload.array('images', 5) allows up to 5 images per product
+router.post('/', protect, admin, upload.array('images', 5), createProduct);
+router.put('/:id', protect, admin, upload.array('images', 5), updateProduct);
+router.delete('/:id', protect, admin, deleteProduct);
+
+module.exports = router;
